537a1507a83c57dff0576de72b3664e5bfa6de35
taylorbailey97 <36019252+taylorbailey97@users.noreply.github.com>
2018-10-20 21:54:51 -0600

    Added try catch block

--- MegaDesk-Ries/DeskQuote.cs
+++ MegaDesk-Ries/DeskQuote.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
+using System.IO;
 
 namespace MegaDesk_Ries
 {
@@ -84,7 +85,102 @@ namespace MegaDesk_Ries
         {
             this.TotalQuote = totalQuote;
         }
+
+        public int calcuateQuote(Desk desk, int rushDays)
+        {
+            // Declare and initialize quote variable with base cost
+            int totalQuote = BASE_DESK_PRICE;
+
+            // Determine desk area cost if any
+            if (desk.getArea() > DESK_AREA_THRESHOLD)
+                totalQuote += desk.getArea() * DESK_AREA_PRICE;
+
+            // Determine drawer cost
+            totalQuote += desk.Drawers * DRAWER_PRICE;
+
+            // Determine material cost using enum values
+            totalQuote += (int)desk.Material;
+
+            totalQuote += getRushOrder(desk, rushDays);
+
+            // total desk quote should be calculated at this point. Return the quote price
+            return totalQuote;
+        }
+
+        public int getRushOrder(Desk desk, int rushDays)
+        {
+            int totalQuote = 0;
+            try
+            {
+                // Read rushOrderPrices.txt into 2D array to refer to for costs
+                int[,] rushOrderValues = new int[3, 3];
+
+
+                string cFile = @"rushOrderPrices.txt";
+                if (!File.Exists(cFile))
+                {       
+                    throw new System.AccessViolationException("rushorderPrices.txt does not exist!");
+                }
+
+                // Read the file into a string called input
+                string[] input = File.ReadAllLines(cFile);
+
+                // Nested 'for' loop to populate 2D array with input
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                        rushOrderValues[i, j] = int.Parse(input[j + (3 * i)]);
+                }                
+                
+                // Determine rush order cost by rush days - 14 days has no additional cost
+                switch (rushDays)
+                {
+                    // 3 day rush order
+                    case 3:
+                        // desk area less than 1000
+                        if (desk.getArea() < 1000)
+                            totalQuote += rushOrderValues[0, 0];
+                        // desk area 1000 to 2000
+                        else if (desk.getArea() >= 1000 && desk.getArea() <= 2000)
+                            totalQuote += rushOrderValues[0, 1];
+                        // desk area greater than 2000
+                        else
+                            totalQuote += rushOrderValues[0, 2];
+                        break;
+                    // 5 day rush order
+                    case 5:
+                        // desk area less than 1000
+                        if (desk.getArea() < 1000)
+                            totalQuote += rushOrderValues[1, 0];
+                        // desk area 1000 to 2000
+                        else if (desk.getArea() >= 1000 && desk.getArea() <= 2000)
+                            totalQuote += rushOrderValues[1, 1];
+                        // desk area greater than 2000
+                        else
+                            totalQuote += rushOrderValues[1, 2];
+                        break;
+                    // 7 day rush order
+                    case 7:
+                        // desk area less than 1000
+                        if (desk.getArea() < 1000)
+                            totalQuote += rushOrderValues[2, 0];
+                        // desk area 1000 to 2000
+                        else if (desk.getArea() >= 1000 && desk.getArea() <= 2000)
+                            totalQuote += rushOrderValues[2, 1];
+                        // desk area greater than 2000
+                        else
+                            totalQuote += rushOrderValues[2, 2];
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR: Exception caught: " + e);
+            }
+            return totalQuote;
+        }
     }
 }
+
 
 
